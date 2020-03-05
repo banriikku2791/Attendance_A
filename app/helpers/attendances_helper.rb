56cdaa8bf438b202c475f6a6aceb081bfabfdbb3
banriikku <banriikku.ts@gmail.com>
2020-03-05 16:30:32 +0000

    heroku_up_確認修正14_勤怠ログ画面における判定不正処理修正(Puts処理除去)

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -258,12 +258,6 @@ module AttendancesHelper
 
   # 終了予定時間（時）
   def working_hours3(base_at, zan_at, ck_tomorrow_flg, target_s, target_f)
-    puts "working_hours3(base_at, zan_at, ck_tomorrow_flg, target_s, target_f)"
-    puts base_at
-    puts zan_at
-    puts ck_tomorrow_flg
-    puts target_s
-    puts target_f
     tmp_time_f = working_time_rule_f(target_s, target_f, nil, nil, nil, false)
     wk_at = working_time_rule_f(target_s, tmp_time_f, base_at, zan_at, ck_tomorrow_flg, true)
     return format('%02d', wk_at.hour)
@@ -271,12 +265,6 @@ module AttendancesHelper
   
   # 終了予定時間（分）
   def working_minutes3(base_at, zan_at, ck_tomorrow_flg, target_s, target_f)
-    puts "working_minutes3(base_at, zan_at, ck_tomorrow_flg, target_s, target_f)"
-    puts base_at
-    puts zan_at
-    puts ck_tomorrow_flg
-    puts target_s
-    puts target_f
     tmp_time_f = working_time_rule_f(target_s, target_f, nil, nil, nil, false)
     wk_at = working_time_rule_f(target_s, tmp_time_f, base_at, zan_at, ck_tomorrow_flg, true)
     return format('%02d', wk_at.min)
